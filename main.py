59aacdd7115215de309eb403ffe9f5fa7ac71522
Elvis Genao <elvisp12353@gmail.com>
2020-05-21 00:14:21 -0400

    agregue el empate en el juego y mejore el tomar fichas

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ selection = "seleccione \n 1 para piedra \n 2 para papel \n 3 para tijeras"
 
 player1 =  objets.player()
 print(selection)
-player1.choice = objets.pieces_list[int(input())]
+player1.pick_piece(int(input()))
 
 
 player2 =  objets.player()
 print(selection)
-player2.choice = objets.pieces_list[int(input())]   
+player2.pick_piece(int(input()))
+   
+
+
+
+
+if player1.choice == player2.choice:
+    player1.status = "draw"
+    player2.status = "draw"
+    print("This game is draw")
 
 
--- objets.py
+++ objets.py
@@ -1,3 +1,16 @@
+#!/ust/bin/python3
+__author__ ="ELvis Genao"
+__copyright__ ="Copyright"
+__credits__ ="Codigo elvis"
+__license__ ="GPl"
+__version__ ="1.0.1"
+__maintainer__ ="Eduardo Ismael"
+__email__ ="elvisp12353@gmail.com"
+__status__ ="Developer"
+
+"""
+Este modulo se encarga de crear las fichas
+"""
 class pieces:
     def __init__(self,value = 0,desc = "none",win_to ="",lose_to=""):
         self.value = value
@@ -5,9 +18,16 @@ class pieces:
         self.win_to = win_to
         self.lose_to = lose_to
 class player:
-    def __init__(self,choice="null",status ="nully"):
+    def __init__(self,choice="null",status ="nully",count=0):
         self.choice = choice
         self.status = status
+        self.count = count
+
+    def pick_piece(self,input):
+        """
+        Esta funcion hace que el usuario escoja una ficha
+        """
+        self.choice = pieces_list[input]
 
 
 
@@ -20,4 +40,3 @@ pieces_list = {
     2:paper.desc,
     3:scissors.desc,
 }
-print(pieces_list[1])
