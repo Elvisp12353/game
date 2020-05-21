ca98d09a9e2060e3f8da85dfe6b8559375aed776
Elvis Genao <elvisp12353@gmail.com>
2020-05-21 14:58:49 -0400

    cambios menores

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import objets 
 selection = "seleccione \n 1 para piedra \n 2 para papel \n 3 para tijeras"
 
-
-
-
 player1 =  objets.player()
 print(selection)
 player1.pick_piece(int(input()))
@@ -13,9 +10,6 @@ player2 =  objets.player()
 print(selection)
 player2.pick_piece(int(input()))
    
-
-
-
 
 if player1.choice == player2.choice:
     player1.status = "draw"
